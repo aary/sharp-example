5d98234e686e6c33baa2e596801a0df8cdf8ec5b
Aaryaman Sagar <rmn100@gmail.com>
2016-09-01 23:30:47 -0400

    [master] Added initial example and submodules

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include "sharp/Tags/Tags.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    __attribute__((unused)) sharp::initializer_list_construct construct;
+    cout << "Successfully included a library and built it!" << endl;
+    return 0;
+}
